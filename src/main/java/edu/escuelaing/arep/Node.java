dfd81badc4cb32c55b9024901a172d91cca3562c
YohannaToro <yoha_duran156@hotmail.com>
2019-08-16 03:35:27 -0500

    finalizado

--- src/main/java/edu/escuelaing/arep/Node.java
+++ src/main/java/edu/escuelaing/arep/Node.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 CrkJo
+ * Copyright (C) 2019 YohannaToro
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
